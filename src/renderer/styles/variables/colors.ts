534b1cd003b5e29d54e6bd95154e694132db9a97
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-26 17:00:21 -0300

    [add] other colors

--- src/renderer/styles/variables/colors.ts
+++ src/renderer/styles/variables/colors.ts
@@ -1,4 +1,7 @@
 export const colors = {
+  white: '#e1e1e1',
+  black: '#000',
+
   'primary-500': '#2926DC',
   'primary-600': '#221FB8',
   'primary-700': '#1A188F',
@@ -12,4 +15,6 @@ export const colors = {
   'tertiary-200': '#A5E096',
   'tertiary-700': '#445C3E',
   'tertiary-800': '#293826',
+
+  // use zinc by default for gray scale
 };
