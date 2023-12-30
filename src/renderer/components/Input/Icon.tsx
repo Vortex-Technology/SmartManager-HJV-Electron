fffc5607038ba0bf2ae003fdbcb0e8e10d8ed72a
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-12-30 17:56:17 -0300

    [update]

--- src/renderer/components/Input/Icon.tsx
+++ src/renderer/components/Input/Icon.tsx
@@ -3,13 +3,13 @@ import { HTMLAttributes } from 'react';
 import { VariantProps, tv } from 'tailwind-variants';
 
 const inputIconStyles = tv({
-  base: 'flex items-center w-3.5 h-3.5 text-zinc-50',
+  base: 'flex items-center w-3.5 h-3.5 text-black',
 });
 
 interface InputIconProps
   extends HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof inputIconStyles> {
-  asChild: boolean;
+  asChild?: boolean;
 }
 
 export function Icon({ asChild = false, className, ...props }: InputIconProps) {
