74c71385a73ffc43c656c8b7dc929fa88f11f22e
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-01-03 16:20:58 -0300

    [update]  import

--- src/renderer/routes/AppRoutes/index.tsx
+++ src/renderer/routes/AppRoutes/index.tsx
@@ -2,9 +2,10 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from '@pages/Home';
 import { usePreload } from '@hooks/usePreload';
 import { CreateAdministratorSessionPage } from '@pages/CreateAdministratorSession';
-import { CreateViewerSessionPage } from '@pages/CreateViewerSession';
+import { CreateAttendantSessionPage } from '@pages/CreateAttendantSession';
+import { CreateSellerSessionPage } from '@pages/CreateSellerSession';
 import { CreateAdministratorRegisterPage } from '@pages/CreateAdministratorRegister';
-import { CreateEmployeeRegisterPage } from '@pages/CreateEmployeRegister';
+import { CreateCollaboratorRegisterPage } from '@pages/CreateCollaboratorRegister';
 
 export function AppRoutes() {
   const { isLoading } = usePreload();
@@ -19,20 +20,24 @@ export function AppRoutes() {
         <Route path="/" element={<HomePage />} />
 
         <Route
-          path="/administrator/login"
-          element={<CreateAdministratorSessionPage />}
-        />
-
-        <Route path="/viewer/login" element={<CreateViewerSessionPage />} />
-
-        <Route
           path="/register/administrator"
           element={<CreateAdministratorRegisterPage />}
         />
 
         <Route
+          path="/administrator/login"
+          element={<CreateAdministratorSessionPage />}
+        />
+
+        <Route
           path="/register/employee"
-          element={<CreateEmployeeRegisterPage />}
+          element={<CreateCollaboratorRegisterPage />}
+        />
+
+        <Route path="/seller/login" element={<CreateSellerSessionPage />} />
+        <Route
+          path="/attendant/login"
+          element={<CreateAttendantSessionPage />}
         />
       </Routes>
     </Router>
