f2850c238b4aeee64dae90303585c406fae2e66b
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-01-03 16:19:11 -0300

    [add] page CreateSellerSession

---
+++ src/renderer/pages/CreateSellerSession/index.tsx
@@ -0,0 +1,18 @@
+import { Logo } from '@components/Logo';
+import { Welcome } from '@components/Welcome';
+import { CreateSessionForm } from './components/CreateSessionForm';
+
+export function CreateSellerSessionPage() {
+  return (
+    <main className="flex justify-between h-screen w-screen max-lg:flex-col max-lg:overflow-y-auto">
+      <div className="bg-tertiary-700 w-full flex flex-col p-16 shadow-2xl gap-6">
+        <Logo />
+        <Welcome />
+      </div>
+
+      <div className="w-full p-10">
+        <CreateSessionForm />
+      </div>
+    </main>
+  );
+}
