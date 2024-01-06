976dc7db45a6fdae108924c96c96339cf2163823
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2024-01-06 20:49:26 -0300

    [add] new abstractions on useRoutes hook

--- src/renderer/hooks/useRoutes.tsx
+++ src/renderer/hooks/useRoutes.tsx
@@ -1,13 +1,23 @@
-import { Navigate, Outlet, useNavigate, useParams } from 'react-router-dom';
+import {
+  Link,
+  Navigate,
+  Outlet,
+  useLocation,
+  useNavigate,
+  useParams,
+} from 'react-router-dom';
 
 export function useRoutes() {
   const navigate = useNavigate();
   const params = useParams();
+  const { pathname } = useLocation();
 
   return {
     navigate,
     params,
     Outlet,
     Navigate,
+    Link,
+    pathname,
   };
 }
