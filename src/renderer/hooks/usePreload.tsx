7025276e00c1cb29fd47931e1a42a01ff3092555
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-01-05 21:25:45 -0300

    [add] hook useAuthenticatedCollaborator inside function

--- src/renderer/hooks/usePreload.tsx
+++ src/renderer/hooks/usePreload.tsx
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
 import { socket } from '@services/socket/socketConfig';
 import { useAdministrator } from './useAdministrator';
+import { useAuthenticatedCollaborator } from './useAuthenticatedCollaborator';
 
 export function usePreload() {
   const { preloadAdministrator, isLoadingAdministrator } = useAdministrator();
   const isLoading = isLoadingAdministrator;
+
+  useAuthenticatedCollaborator();
 
   useEffect(() => {
     preloadAdministrator();
