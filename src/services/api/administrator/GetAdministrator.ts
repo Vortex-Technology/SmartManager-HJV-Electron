31e28f99964fc86df3ebff47bfe74d17cab9a430
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-01-05 21:32:13 -0300

    [refactor] function getAdministratorRequest

--- src/services/api/administrator/GetAdministrator.ts
+++
@@ -1,7 +0,0 @@
-import { connection } from '@services/axios-config';
-
-export async function getAdministrator() {
-  const response = await connection.get('/administrator');
-
-  return response;
-}
