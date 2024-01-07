39b0be827ec57bbad79133de988517a5758f036c
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-01-07 14:32:20 -0300

    [add] function deleteAdministratortRequest

---
+++ src/services/api/administrator/DeleteAdministratorRequest.ts
@@ -0,0 +1,7 @@
+import { connection } from '@services/axios-config';
+
+export async function deleteAdministratorRequest(id: string) {
+  const response = await connection.delete(`administrator/${id}`);
+
+  return response;
+}
