ba2a89c2a33cfb6fee550a0436fa4794480d9bcc
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-01-10 17:12:37 -0300

    [add] listCollaboratorsFake

--- src/renderer/pages/Collaborator/DeleteCollaborator/index.tsx
+++ src/renderer/pages/Collaborator/DeleteCollaborator/index.tsx
@@ -3,6 +3,8 @@ import { Button } from '@components/Button';
 import { useRoutes } from '@hooks/useRoutes';
 import { useDeleteCollaborator } from '@hooks/useDeleteCollaborator';
 import { useAuthenticatedCollaborator } from '@hooks/useAuthenticatedCollaborator';
+import { Collaborator } from '@entities/Collaborator';
+import { getCollaborators } from '../../../../listCollaboratorsFake/mockCollaboratorsFake';
 
 export function DeleteCollaboratorPage() {
   const { handleDeleteAdministrator } = useDeleteCollaborator();
@@ -15,9 +17,36 @@ export function DeleteCollaboratorPage() {
     return <Page404 />;
   }
 
+  const allCollaborators: Collaborator[] = getCollaborators();
+
+  if (!allCollaborators) {
+    return <Page404 />;
+  }
+
   return (
-    <div>
+    <div className="flex  flex-wrap gap-5">
       <span>Excluir um Administrator</span>
+      <div className="flex flex-wrap gap-5">
+        {allCollaborators.map((collaborator) => (
+          <div
+            key={collaborator.id}
+            className="border-spacing-1 p-8 w-48 box-border"
+            role="button"
+            tabIndex={0}
+            onClick={() => handleDeleteAdministrator(collaborator.id)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' || e.key === ' ') {
+                handleDeleteAdministrator(collaborator.id);
+              }
+            }}
+          >
+            <div>ID: {collaborator.id}</div>
+            <div>Nome: {collaborator.name}</div>
+            <div>Alt da imagem: {collaborator.image.alt}</div>
+            <div>URL da imagem: {collaborator.image.url}</div>
+          </div>
+        ))}
+      </div>
 
       <div>você realmente deseja excluir esse funcionário?</div>
 
